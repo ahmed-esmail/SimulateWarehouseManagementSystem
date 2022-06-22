9f019bcbf7c38b5eca08a78544c63620cef9b2a4
ahmed.esmail <ahmed.esmail.gm@gmail.com>
2022-06-22 15:10:19 +0200

    Finished

--- HardwareWarehouse MS/Program.cs
+++ HardwareWarehouse MS/Program.cs
@@ -6,46 +6,62 @@ namespace HardwareWarehouse_MS
   internal class Program
   {
     private const int BatchSize = 5;
+
     public static void Main(string[] args)
     {
       var hardwareItemQueue = new CustomQueue<HardwareItem>();
       hardwareItemQueue.CustomQueueEvent += HardwareItemQueueOnCustomQueueEvent;
-      
+
+      AddItemsToQueue(hardwareItemQueue);
+
+      Console.ReadKey();
+    }
+
+    private static void AddItemsToQueue(CustomQueue<HardwareItem> hardwareItemQueue)
+    {
       //comes into stock - device scans a bar code or QR code
-      hardwareItemQueue.AddItem(new Drill { Id = 1, Name = "Drill 1", Type = "Drill", UnitValue = 20.00m, Quantity = 10 });
+      hardwareItemQueue.AddItem(new Drill
+        {Id = 1, Name = "Drill 1", Type = "Drill", UnitValue = 20.00m, Quantity = 10});
 
       System.Threading.Thread.Sleep(1000);
 
-      hardwareItemQueue.AddItem(new Drill { Id = 2, Name = "Drill 2", Type = "Drill", UnitValue = 30.00m, Quantity = 20 });
+      hardwareItemQueue.AddItem(new Drill
+        {Id = 2, Name = "Drill 2", Type = "Drill", UnitValue = 30.00m, Quantity = 20});
 
       System.Threading.Thread.Sleep(2000);
 
-      hardwareItemQueue.AddItem(new Ladder { Id = 3, Name = "Ladder 1", Type = "Ladder", UnitValue = 100.00m, Quantity = 5 });
+      hardwareItemQueue.AddItem(new Ladder
+        {Id = 3, Name = "Ladder 1", Type = "Ladder", UnitValue = 100.00m, Quantity = 5});
 
       System.Threading.Thread.Sleep(1000);
 
-      hardwareItemQueue.AddItem(new Hammer { Id = 4, Name = "Hammer 1", Type = "Hammer", UnitValue = 10.00m, Quantity = 80 });
+      hardwareItemQueue.AddItem(new Hammer
+        {Id = 4, Name = "Hammer 1", Type = "Hammer", UnitValue = 10.00m, Quantity = 80});
       System.Threading.Thread.Sleep(3000);
 
-      hardwareItemQueue.AddItem(new PaintBrush { Id = 5, Name = "Paint Brush 1", Type = "PaintBrush", UnitValue = 5.00m, Quantity = 100 });
+      hardwareItemQueue.AddItem(new PaintBrush
+        {Id = 5, Name = "Paint Brush 1", Type = "PaintBrush", UnitValue = 5.00m, Quantity = 100});
       System.Threading.Thread.Sleep(3000);
 
-      hardwareItemQueue.AddItem(new PaintBrush { Id = 6, Name = "Paint Brush 2", Type = "PaintBrush", UnitValue = 5.00m, Quantity = 100 });
+      hardwareItemQueue.AddItem(new PaintBrush
+        {Id = 6, Name = "Paint Brush 2", Type = "PaintBrush", UnitValue = 5.00m, Quantity = 100});
       System.Threading.Thread.Sleep(3000);
 
-      hardwareItemQueue.AddItem(new PaintBrush { Id = 7, Name = "Paint Brush 3", Type = "PaintBrush", UnitValue = 5.00m, Quantity = 100 });
+      hardwareItemQueue.AddItem(new PaintBrush
+        {Id = 7, Name = "Paint Brush 3", Type = "PaintBrush", UnitValue = 5.00m, Quantity = 100});
       System.Threading.Thread.Sleep(3000);
 
-      hardwareItemQueue.AddItem(new Hammer { Id = 8, Name = "Hammer 2", Type = "Hammer", UnitValue = 11.00m, Quantity = 80 });
+      hardwareItemQueue.AddItem(new Hammer
+        {Id = 8, Name = "Hammer 2", Type = "Hammer", UnitValue = 11.00m, Quantity = 80});
       System.Threading.Thread.Sleep(3000);
 
-      hardwareItemQueue.AddItem(new Hammer { Id = 9, Name = "Hammer 3", Type = "Hammer", UnitValue = 13.00m, Quantity = 80 });
+      hardwareItemQueue.AddItem(new Hammer
+        {Id = 9, Name = "Hammer 3", Type = "Hammer", UnitValue = 13.00m, Quantity = 80});
       System.Threading.Thread.Sleep(3000);
 
-      hardwareItemQueue.AddItem(new Hammer { Id = 10, Name = "Hammer 4", Type = "Hammer", UnitValue = 14.00m, Quantity = 80 });
+      hardwareItemQueue.AddItem(new Hammer
+        {Id = 10, Name = "Hammer 4", Type = "Hammer", UnitValue = 14.00m, Quantity = 80});
       System.Threading.Thread.Sleep(3000);
-
-      Console.ReadKey();
     }
 
     private static void HardwareItemQueueOnCustomQueueEvent(CustomQueue<HardwareItem> sender, QueueEventArgs eventArgs)
@@ -71,22 +87,20 @@ namespace HardwareWarehouse_MS
         {
           ProcessItems(sender);
         }
-
       }
       else
       {
         Console.WriteLine("Status: All items have been processed.");
       }
     }
+
     private static void ProcessItems(CustomQueue<HardwareItem> customQueue)
     {
       while (customQueue.QueueLength > 0)
       {
-        System.Threading.Thread.Sleep(3000); 
-        HardwareItem hardWareItem = customQueue.GetItem(); 
-
+        System.Threading.Thread.Sleep(3000);
+        HardwareItem hardWareItem = customQueue.GetItem();
       }
-
     }
   }
 }
